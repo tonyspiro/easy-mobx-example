1312bbdfaf3eca46488c40a9add671a92691d271
Tony Spiro <tspiro@tonyspiro.com>
2016-06-22 13:47:46 -0500

    :rocket:

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET)
     })
   ]
 }
